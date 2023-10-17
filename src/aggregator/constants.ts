68d933ef55cc5b76e59a78030c772584f6fb2ac6
Tony <ctonydev@gmail.com>
2023-10-17 12:16:18 -0400

    add gmx contract addresses

--- src/aggregator/constants.ts
+++ src/aggregator/constants.ts
@@ -11,6 +11,26 @@ export const CHAIN_ID_TO_AGGREGATOR_FACTORY_ADDRESS: { [chainID: number]: string
   42161: '0x2ff2f1D9826ae2410979ae19B88c361073Ab0918'
 }
 
+export const CHAIN_ID_TO_AGGREGATOR_VAULT_ADDRESS: { [chainID: number]: string } = {
+  // arb1
+  42161: '0x489ee077994B6658eAfA855C308275EAd8097C4A'
+}
+
+export const CHAIN_ID_TO_AGGREGATOR_POSITION_ROUTER_ADDRESS: { [chainID: number]: string } = {
+  // arb1
+  42161: '0xb87a436B93fFE9D75c5cFA7bAcFff96430b09868'
+}
+
+export const CHAIN_ID_TO_AGGREGATOR_ORDER_BOOK_ADDRESS: { [chainID: number]: string } = {
+  // arb1
+  42161: '0x09f77E8A13De9a35a7231028187e9fD5DB8a2ACB'
+}
+
+export const CHAIN_ID_TO_AGGREGATOR_ROUTER_ADDRESS: { [chainID: number]: string } = {
+  // arb1
+  42161: '0xaBBc5F99639c9B6bCb58544ddf04EFA6802F4064'
+}
+
 export const CHAIN_ID_TO_AGGREGATOR_SHORT_FUNDING_ASSET_ID: { [chainID: number]: number } = {
   42161: 3
 }
